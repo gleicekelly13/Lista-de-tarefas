091fdc68e21ab3f7576f674d0908c47d8303fb34
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-10-05 17:54:00 -0300

    Adicionando o valor do input no item da lista
    lista.innerHTML: Não é muito eficiente pq é como se ele readicionasse todos os itens já adicionados

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const input = document.querySelector('input');
 const lista = document.querySelector('ul');
+
+
+function handleKeyUp(e) {
+    if(e.key === 'Enter') {
+        lista.innerHTML += '<li>'+ input.value + '</li'; //Pega o conteúdo de lista interno e adiciona o valor do input
+    }
+}
+
+input.addEventListener('keyup', handleKeyUp);
+console.log(input.value)
