0f6d2b5373a2fa0b1c2164d7b06f3d8994b660c1
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-10-19 14:42:28 -0300

    Alteração na function handleKeyUp para exibir a tarefa e o input
    Com a alteração no script, o checkbox será adicionada dinamicamente dentro do elemento li, em vez de ser adicionado separadamente.

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,10 +6,19 @@ const lista = document.querySelector('ul');
 function handleKeyUp(e) { //Função que lida com o evento de soltar uma tecla
     if(e.key === 'Enter') { //Verifica se o usuário digitou a tecla 'Enter'
         const newLi = document.createElement('li'); //Cria um novo elemento, neste caso, um novo item da lista, e armazena em newLi
-        newLi.innerHTML = input.value;  //Coloca o texto digitado no input dentro do novo elemento(newLi)
-        lista.appendChild(newLi); //Adiciona o elemento novo à tela, dentro da lista
 
-        input.value = ''; //Limpa o que foi digitado no campo de texto
+        const checkbox = document.createElement('input'); //Cria o checkbox
+        checkbox.type = 'checkbox';
+
+        newLi.appendChild(checkbox); //Adiciona o checkbox ao item da lista
+
+        const texto_tarefa = document.createElement('span'); //Cria um elemento <span> para o texto da tarefa
+        texto_tarefa.innerHTML = input.value;
+
+        newLi.appendChild(texto_tarefa); //Adiciona o texto da tarefa ao item da lista
+        lista.appendChild(newLi); //Adiciona o item à lista
+
+        input.value = ''; //Limpa o que foi digitado no campo de texto 
     }
 }
 
